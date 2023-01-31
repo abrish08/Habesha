1c894a0e8aef27b73e7aef309a6a88e20f81dfff
abrish08 <98913823+abrish08@users.noreply.github.com>
2023-01-31 14:53:41 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Habesha</a>
+      </header>
+      <main>Available Products</main>
+    </div>
+  );
+}
+
+export default App;
